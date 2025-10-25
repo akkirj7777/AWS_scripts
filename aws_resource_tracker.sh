8e82c3fe190dba0263d7ab796aa2fee0398ea9e1
root <akkirj7777@gmail.com>
2025-10-25 11:16:37 +0530

    first shell script

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#######################
+# Author: Akshay
+# Date: 25th-Oct
+# Version: V1
+#
+# This script will report the AWS resoruce usage
+#######################
+
+set +x
+
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM users
+#
+
+# list s3 buckets
+echo "print list of aws buckets"
+aws s3 ls
+
+# list EC2 instances
+echo "print list of aws ec2 instances"
+aws ec2 describe-instances
+
+# list Lambda
+echo "print list of aws lambda functions"
+
+aws lambda list-functions
+
+# list IAM users
+echo "print list of aws IAM users"
+
+aws iam list-users
+
